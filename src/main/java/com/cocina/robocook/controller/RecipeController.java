bf8da46705558d21cbdf6420f963a8068331f2ee
Ivan Souto <isgroba@gmail.com>
2026-03-05 13:21:22 +0100

    modifica save con separacion de objetoForm y objetoBD

--- src/main/java/com/cocina/robocook/controller/RecipeController.java
+++ src/main/java/com/cocina/robocook/controller/RecipeController.java
@@ -1,22 +1,28 @@
 package com.cocina.robocook.controller;
 
-import com.cocina.robocook.dao.RecipeRepository;
 import com.cocina.robocook.entity.*;
+import com.cocina.robocook.service.CategoryService;
+import com.cocina.robocook.service.LabelService;
 import com.cocina.robocook.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("/recipes")
 public class RecipeController {
     private RecipeService recipeService;
+    private LabelService labelService;
+    private CategoryService categoryService;
 
-    public RecipeController(RecipeService service) {
+    public RecipeController(RecipeService service, LabelService labelService, CategoryService categoryService) {
         this.recipeService = service;
+        this.labelService = labelService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/list")
@@ -50,9 +56,46 @@ public class RecipeController {
     }
 
     @PostMapping("/save")
-    public String saveRecipe(@ModelAttribute("theRecipe") Recipe theRecipe){
+    public String saveRecipe(@ModelAttribute("theRecipe") Recipe formRecipe){
+        Recipe recipeDB;
+        List<Label> labels = new ArrayList<>();
+        List<Category> categories = new ArrayList<>();
+        Date today = new Date();
 
-        recipeService.save(theRecipe);
+        if(formRecipe.getId() != null){
+            recipeDB = recipeService.findById(formRecipe.getId());
+        }else{
+            recipeDB = new Recipe();
+        }
+
+        recipeDB.setId(formRecipe.getId());
+        recipeDB.setName(formRecipe.getName());
+        recipeDB.setDescription(formRecipe.getDescription());
+        recipeDB.setPreparationTime(formRecipe.getPreparationTime());
+        recipeDB.setDifficulty(formRecipe.getDifficulty());
+        recipeDB.setSeason(formRecipe.getSeason());
+        recipeDB.setHealthyScore(formRecipe.getHealthyScore());
+        recipeDB.setTasteScore(formRecipe.getTasteScore());
+        recipeDB.setPortions(formRecipe.getPortions());
+        recipeDB.setCalories(formRecipe.getCalories());
+
+        if(null != recipeDB.getLabels())
+            recipeDB.getLabels().clear();
+        if(null != formRecipe.getLabels())
+            for(Label l: formRecipe.getLabels()){
+                Label label = labelService.findById(l.getId());
+                recipeDB.addLabel(label);
+            }
+
+        if(null != recipeDB.getCategories())
+            recipeDB.getCategories().clear();
+        if(null != formRecipe.getCategories())
+            for(Category c: formRecipe.getCategories()){
+                Category category = categoryService.findById(c.getId());
+                recipeDB.addCategory(category);
+            }
+
+        recipeService.save(recipeDB);
         return "redirect:/recipes/list";
     }
 
